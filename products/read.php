4bf6ecb11b8451e357cd90527d2cf8337a2e4892
Arash Tajdar <a.tajdar@sadad.co.ir>
2019-03-03 13:01:54 +0330

    Error handling for read -> DONE

--- products/read.php
+++ products/read.php
@@ -9,37 +9,33 @@ $connection = $dbclass->connect();
 
 $product = new Product($connection);
 
-$length = !empty($_GET['length']) ? $_GET['length'] : 10;
-$start = !empty($_GET['start']) ? $_GET['start'] : 0;
-$stmt = $product->read($start, $length);
-// echo $stmt;
-// exit;
-$count = $stmt->rowCount();
-if ($count > 0) {
-
-
+try{
+    $length = !empty($_GET['length']) ? $_GET['length'] : 10;
+    $start = !empty($_GET['start']) ? $_GET['start'] : 0;
+    $stmt = $product->read($start, $length);
+    $count = $stmt->rowCount();
+    if(!$count){
+        throw new Exception("No result found !");
+    }
     $products = array();
     $products["body"] = array();
     $products["count"] = $count;
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // var_dump($row);
         extract($row);
-
         $p = array(
             "ID" => $ID,
             "NAME" => $NAME,
             "PRICE" => $PRICE
         );
-
         array_push($products["body"], $p);
     }
-
     echo json_encode($products);
-} else {
-
+}catch (Exception $e){
+    array_push($error, $e->getMessage());
     echo json_encode(
-        array("body" => array(), "count" => 0)
+        array("ERROR" => $error)
     );
 }
+
 ?>
