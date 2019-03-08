5405d40affb0cc6b68c9efcea60b7f06e41f7dd2
arash.tajdar@gmail.com <wnto123wnto123>
2019-03-09 00:15:40 +0330

    crate bash shell script file for swagger just open terminal and type ./runSwagger.sh to update the swagger documentation

---
+++ documentation/runSwagger.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+php vendor/zircote/swagger-php/bin/swagger ../products
--- products/api.php
+++ products/api.php
@@ -1,7 +1,7 @@
 <?php
  /**
  * @SWG\Info(
- *     description="This is a sample php api",
+ *     description="This is a sample php api for Erply assignment",
  *     version="1.0.0",
  *     title="PHP SAMPLE API",
  *     @SWG\Contact(
@@ -12,7 +12,7 @@
 /**
  * @SWG\Tag(
  *     name="products",
- *     description="Acces to products",
+ *     description="Acces to products list and do CRUD",
  * )
 
 
